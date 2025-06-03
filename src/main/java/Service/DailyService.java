36e6b3c7f8202db271763a9e7108a4560aaa7cc1
Cédric Leonel <cedricleonelkouam@gmail.com>
2025-06-04 00:03:02 +0200

    [feature] - implement method using TDD

--- src/main/java/Service/DailyService.java
+++ src/main/java/Service/DailyService.java
@@ -1,7 +1,13 @@
 package Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DailyService {
+
+    private final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MM yyyy");
+
     public String getCurrentDate() {
-        return null;
+        return LocalDate.now().getDayOfWeek().toString() + " " + LocalDate.now().format(dateFormat);
     }
 }
